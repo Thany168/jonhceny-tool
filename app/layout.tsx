566e6665065089ac0859ee533bf77669a66d6bab
Thany168 <ounthany5@gmail.com>
2026-06-04 11:45:45 +0700

    icon up

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,17 @@ export const metadata: Metadata = {
   icons: {
     icon: "/favicon.png",
   },
+  openGraph: {
+    title: "Compress & Resize by Jonhceny",
+    description: "Assist for daily work",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
